bf491b43d27b41d61e0875c216a5b91de744c34a
ederzin13 <edermurilo131313@gmail.com>
2025-02-17 20:46:42 -0300

    fix clientScreen

--- dist/view/ClientScreen.js
+++ dist/view/ClientScreen.js
@@ -9,21 +9,14 @@ class ClientScreen {
         this.prompt = (0, prompt_sync_1.default)();
         this.router = router;
     }
-    // public registerEscola(): void {
-    //     let school: Escola = this.router.schoolController.getNewSchool();
-    //     let schoolName = this.prompt("Qual o nome da escola?");
-    //     school.setName(schoolName);
-    //     let schoolPhone = this.prompt("Qual o número de telefone?");
-    //     school.setPhone(+schoolPhone);
-    //     let schoolCnpj = this.prompt("Qual o CNPJ?");
-    //     school.setCnpj(+schoolCnpj);
-    //     this.router.schoolController.addNewSchool(school);
-    //     console.log("Escola adicionada");
-    // }
     registerPessoa() {
         let person = this.router.clientController.getNewPessoa();
         let clientName = this.prompt("Qual o nome do cliente?");
         person.setName(clientName.toUpperCase());
+        let clientPhone = this.prompt("Qual o número de telefone?");
+        person.setPhone(+clientPhone);
+        let clientCpf = this.prompt("Qual o CPF?");
+        person.setCpf(+clientCpf);
         this.router.clientController.addNewClient(person);
         console.log("Cliente adicionado");
     }
--- src/view/ClientScreen.ts
+++ src/view/ClientScreen.ts
@@ -1,6 +1,7 @@
 import PromptSync from "prompt-sync";
 import Router from "../control/Router";
 import Client from "../model/Cliente";
+import Pessoa from "../model/Pessoa";
 import Escola from "../model/Escola";
 import Endereco from "../model/Endereco";
 
@@ -13,10 +14,16 @@ export default class ClientScreen {
     }
 
     public registerPessoa(): void {
-        let person: Client = this.router.clientController.getNewPessoa();
+        let person: Pessoa = this.router.clientController.getNewPessoa();
         
         let clientName = this.prompt("Qual o nome do cliente?");
         person.setName(clientName.toUpperCase());
+
+        let clientPhone = this.prompt("Qual o número de telefone?");
+        person.setPhone(+clientPhone);
+
+        let clientCpf = this.prompt("Qual o CPF?");
+        person.setCpf(+clientCpf);
         
         this.router.clientController.addNewClient(person);
         console.log("Cliente adicionado");
